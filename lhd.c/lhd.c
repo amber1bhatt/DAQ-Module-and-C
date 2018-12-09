67833504f5756f738b1d828d923b235a24959fb1
amber1bhatt <43303850+amber1bhatt@users.noreply.github.com>
2018-12-08 23:54:11 -0800

    Add files via upload

---
+++ lhd.c/lhd.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+
+int main(void) {
+
+	char *str = "65 + 9 - 4";
+
+	system("pause");
+	return 0;
+}
---
+++ nullStrings/nullStrings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	char source[12] = "0123456789";
+	printf("%s", source);
+
+	system("PAUSE");
+	return 0;
+}
